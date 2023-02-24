818f4978f9e7182340af281ec4d4004fe35caf1b
jfmonsa <juanmonsalve@protonmail.com>
2023-02-24 15:29:01 -0500

    #30 autopairs nvim

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -15,13 +15,13 @@ if fn.empty(fn.glob(install_path)) > 0 then
   vim.cmd [[packadd packer.nvim]]
 end
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]]
+-- -- Autocommand that reloads neovim whenever you save the plugins.lua file
+-- vim.cmd [[
+--   augroup packer_user_config
+--     autocmd!
+--     autocmd BufWritePost plugins.lua source <afile> | PackerSync
+--   augroup end
+-- ]]
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
@@ -44,6 +44,7 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
   
   -- Colorschemes
   --use "https://github.com/folke/tokyonight.nvim"
@@ -55,11 +56,12 @@ return packer.startup(function(use)
   use 'nvim-telescope/telescope-media-files.nvim'
 
   -- Treesitter
-  use {
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-  }
-
+--use {
+--	run = function()
+--		local ts_update = require('nvim-treesitter.install').update({with_sync = true })
+--		ts_update()
+--	end,
+--	}
   use "p00f/nvim-ts-rainbow"
   -- use "nvim-treesitter/playground" <- see sintax tree
   -- cmp plugins
