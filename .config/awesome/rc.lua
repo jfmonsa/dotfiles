ba5af8c6e23dca78e7f0a0acd5ec0cb11cb19cc0
jfmonsa <juanmonsalve@protonmail.com>
2025-01-09 10:27:36 -0500

    fix: Comment out unused gap setting in AwesomeWM configuration

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -579,5 +579,5 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
---- &&&& gaps &&&&
-beautiful.useless_gap = 8
+-- &&&& gaps &&&&
+-- beautiful.useless_gap = 8
